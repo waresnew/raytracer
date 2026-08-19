c23bd2a26834bca2da55ac1a79e0084304537f22
newwares <45801973+waresnew@users.noreply.github.com>
2026-08-18 20:21:17 -0400

    cornell box contents

--- src/scenes.rs
+++ src/scenes.rs
@@ -1,13 +1,16 @@
 use crate::{
     camera::CameraConfig,
-    hittable::Hittable,
+    hittable::{Hittable, parallelogram::Parallelogram},
+    material::Material,
     renderer::RenderConfig,
+    rgb::Rgb,
     scenes::{
         cornell_box::load_cornell_box, mixed_light::load_mixed_light,
         random_balls::load_random_balls,
     },
 };
 use clap::ValueEnum;
+use glam::Vec3;
 
 mod cornell_box;
 mod mixed_light;
@@ -31,4 +34,59 @@ pub fn load_scene(scene: SceneType) -> Scene {
         SceneType::RandomBalls => load_random_balls(),
         SceneType::MixedLight => load_mixed_light(),
     }
+}
+/// yaw in degrees, +ve means ccw
+pub(super) fn make_box(material: Material, centre: Vec3, yaw: f32, scale: Vec3) -> [Hittable; 6] {
+    let mut base = [
+        // bottom face
+        (
+            Vec3::new(-0.5, -0.5, -0.5),
+            Vec3::new(1.0, 0.0, 0.0),
+            Vec3::new(0.0, 0.0, 1.0),
+        ),
+        // top face
+        (
+            Vec3::new(-0.5, 0.5, -0.5),
+            Vec3::new(1.0, 0.0, 0.0),
+            Vec3::new(0.0, 0.0, 1.0),
+        ),
+        // left face
+        (
+            Vec3::new(-0.5, -0.5, -0.5),
+            Vec3::new(0.0, 1.0, 0.0),
+            Vec3::new(0.0, 0.0, 1.0),
+        ),
+        // right face
+        (
+            Vec3::new(0.5, -0.5, -0.5),
+            Vec3::new(0.0, 1.0, 0.0),
+            Vec3::new(0.0, 0.0, 1.0),
+        ),
+        // front face
+        (
+            Vec3::new(-0.5, -0.5, 0.5),
+            Vec3::new(1.0, 0.0, 0.0),
+            Vec3::new(0.0, 1.0, 0.0),
+        ),
+        // back face
+        (
+            Vec3::new(-0.5, -0.5, -0.5),
+            Vec3::new(1.0, 0.0, 0.0),
+            Vec3::new(0.0, 1.0, 0.0),
+        ),
+    ];
+    for (p1, p2, p3) in &mut base {
+        *p1 *= scale;
+        *p2 *= scale;
+        *p3 *= scale;
+
+        *p1 = p1.rotate_y(yaw.to_radians());
+        *p2 = p2.rotate_y(yaw.to_radians());
+        *p3 = p3.rotate_y(yaw.to_radians());
+
+        *p1 += centre;
+    }
+    base.map(|(start, side1, side2)| {
+        Hittable::Parallelogram(Parallelogram::new(start, side1, side2, material))
+    })
 }
--- src/scenes/cornell_box.rs
+++ src/scenes/cornell_box.rs
@@ -6,7 +6,7 @@ use crate::{
     material::{Material, diffuse::Diffuse, diffuse_light::DiffuseLight},
     renderer::RenderConfig,
     rgb::Rgb,
-    scenes::Scene,
+    scenes::{Scene, make_box},
 };
 
 pub fn load_cornell_box() -> Scene {
@@ -36,14 +36,8 @@ pub fn load_cornell_box() -> Scene {
         Vec3::new(0.0, 0.0, -100.0),
         Material::Diffuse(Diffuse::new(Rgb::WHITE)),
     ));
-    let front_wall = Hittable::Parallelogram(Parallelogram::new(
+    let back_wall = Hittable::Parallelogram(Parallelogram::new(
         Vec3::new(-50.0, 0.0, -100.0),
-        Vec3::new(100.0, 0.0, 0.0),
-        Vec3::new(0.0, 100.0, 0.0),
-        Material::Diffuse(Diffuse::new(Rgb::WHITE)),
-    ));
-    let behind_wall = Hittable::Parallelogram(Parallelogram::new(
-        Vec3::new(-50.0, 0.0, 0.0),
         Vec3::new(100.0, 0.0, 0.0),
         Vec3::new(0.0, 100.0, 0.0),
         Material::Diffuse(Diffuse::new(Rgb::WHITE)),
@@ -58,15 +52,30 @@ pub fn load_cornell_box() -> Scene {
             LIGHT_STRENGTH,
         ))),
     ));
-    let objects: Vec<Hittable> = vec![
+
+    let tall_box = make_box(
+        Material::Diffuse(Diffuse::new(Rgb::WHITE)),
+        Vec3::new(-15.0, 30.0, -60.0),
+        20.0,
+        Vec3::new(30.0, 60.0, 30.0),
+    );
+    let cube_box = make_box(
+        Material::Diffuse(Diffuse::new(Rgb::WHITE)),
+        Vec3::new(15.0, 15.0, -30.0),
+        -20.0,
+        Vec3::new(30.0, 30.0, 30.0),
+    );
+
+    let mut objects: Vec<Hittable> = vec![
         left_wall,
         right_wall,
         floor,
         ceiling,
-        front_wall,
-        behind_wall,
+        back_wall,
         light_source,
     ];
+    objects.extend(tall_box);
+    objects.extend(cube_box);
     Scene {
         objects,
         render_config: RenderConfig {
@@ -75,9 +84,9 @@ pub fn load_cornell_box() -> Scene {
             sky_colour: Rgb::BLACK,
         },
         camera_config: CameraConfig {
-            centre: Vec3::new(0.0, 50.0, -10.0),
-            look_at_centre: Vec3::new(0.0, 50.0, -30.0),
-            vertical_fov: 90.0,
+            centre: Vec3::new(0.0, 50.0, 137.4), // 50/tan(40/2)
+            look_at_centre: Vec3::new(0.0, 50.0, -50.0),
+            vertical_fov: 40.0,
             lens_radius: 0.0,
         },
     }
